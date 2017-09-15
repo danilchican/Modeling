a9625c6a73a8b0cccdbd8f045931750ca47c6559
danilchican <danilchican@mail.ru>
2017-09-15 13:12:02 +0300

    Fixed EvenGenerator

--- lab2/src/main/java/com/bsuir/danilchican/generator/EvenGenerator.java
+++ lab2/src/main/java/com/bsuir/danilchican/generator/EvenGenerator.java
@@ -3,11 +3,6 @@ package com.bsuir.danilchican.generator;
 public class EvenGenerator extends Generator {
 
     @Override
-    protected boolean isValidValues() {
-        return false;
-    }
-
-    @Override
     public void generate() {
 
     }
@@ -18,17 +13,22 @@ public class EvenGenerator extends Generator {
     }
 
     @Override
+    public void input() {
+
+    }
+
+    @Override
+    public void print() {
+
+    }
+
+    @Override
+    protected boolean isValidValues() {
+        return false;
+    }
+
+    @Override
     protected double getRandomNumber() {
         return 0;
-    }
-
-    @Override
-    protected void input() {
-
-    }
-
-    @Override
-    protected void print() {
-
     }
 }
